ce1dc290405b1e62c04802ce03fc6667d59b8c6a
huygiavu <huyvugia2108@gmail.com>
2022-09-07 21:41:13 +0700

    Fixed Openwebcam

--- OpenWebcam.py
+++ OpenWebcam.py
@@ -1,11 +1,12 @@
 import cv2
 
 webcam = cv2.VideoCapture(0)
+
 while True:
     _, img = webcam.read()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-    cv2.imshow('Frame', img)
+    cv2.imshow('Webcam', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
