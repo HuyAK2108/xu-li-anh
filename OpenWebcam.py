5aeb9a28abf9aa6cfdef7c03e90fe1fce451fad7
huygiavu <huyvugia2108@gmail.com>
2022-09-17 15:16:26 +0700

    Update code

--- OpenWebcam.py
+++ OpenWebcam.py
@@ -6,8 +6,13 @@ while True:
     _, img = webcam.read()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    fps = webcam.get(cv2.CAP_PROP_FPS)
+    text = "FPS: {:.2f}".format(fps) 
+    cv2.putText(img, text, (5, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255), 1)
     cv2.imshow('Webcam', img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    print(fps)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) == 27:
         break
     
 webcam.release()
